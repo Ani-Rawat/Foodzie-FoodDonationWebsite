2a171dc4b5d1395f1127d65df99609872c7cf34b
Anisha Rawat <anisharawat1405@gmail.com>
2023-02-11 14:32:47 +0530

    Bulk commiting files from Local

---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Join Us</h2>
+          <p>
+          “One person can make a difference, and everyone should try. You make a difference. You can make the organization better; you can be part of changing the world.”
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <a href={d.link}><i className={d.icon}></i></a>
+                  <div className='service-desc'>
+                    <a href={d.link}><h3>{d.name}</h3></a>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
